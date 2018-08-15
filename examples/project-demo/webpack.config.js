1210a93c48dc3387e31d7b8d796e388eebf388a3
evan <redwolf0302@gmail.com>
2018-08-15 10:56:49 +0800

    maintain css bug.

--- examples/project-demo/webpack.config.js
+++ examples/project-demo/webpack.config.js
@@ -1,6 +1,9 @@
 const path = require('path');
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
 
 const sourcePath = path.resolve('./src');
 const distPath = path.resolve('./dist');
@@ -32,7 +35,14 @@ module.exports = {
         rules: [
             {
                 test: /\.css$/,
-                use: 'css-loader'
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                        options: {
+                            publicPath: '../'
+                        }
+                    },
+                    'css-loader']
             },
             {
                 test: /\.jsx?$/,
@@ -57,6 +67,16 @@ module.exports = {
             'node_modules'
         ]
     },
+    optimization: {
+        minimizer: [
+            new UglifyJsPlugin({
+                cache: true,
+                parallel: true,
+                sourceMap: true
+            }),
+            new OptimizeCSSAssetsPlugin({})
+        ]
+    },
     plugins: [
         new webpack.LoaderOptionsPlugin({
             debug: true
@@ -64,10 +84,14 @@ module.exports = {
         new webpack.DefinePlugin({
             'process.env.NODE_ENV': '"development"'
         }),
+        new MiniCssExtractPlugin({
+            filename: "[name].[hash].css",
+            chunkFilename: "[id].[hash].css"
+        }),
         new webpack.HotModuleReplacementPlugin(),
         new webpack.NoEmitOnErrorsPlugin(),
         new HtmlWebpackPlugin({
-            template:`${sourcePath}/index.html`,
+            template: `${sourcePath}/index.html`,
             minify: {
                 collapseWhitespace: true,
                 conservativeCollapse: true,
