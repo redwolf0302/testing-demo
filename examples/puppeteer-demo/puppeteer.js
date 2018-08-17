466c1a48a8e9995564b0ec113f217315fcfd681c
evan <redwolf0302@gmail.com>
2018-08-17 15:06:49 +0800

    test puppeteer

--- examples/puppeteer-demo/puppeteer.js
+++ examples/puppeteer-demo/puppeteer.js
@@ -1,11 +1,16 @@
 let url = 'http://localhost:8080/';
+const fs = require('fs');
 const puppeteer = require('puppeteer');
-
+if (!fs.existsSync('dist')) {
+    fs.mkdirSync('dist');
+}
 (async () => {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
     page.setViewport({width: 414, height: 736, isMobile: true});
     await page.goto(url);
-    await page.screenshot({path: 'consult_41963767.png'});
+    await page.click('.home-enter-btn');
+    await page.click('.card.can-click');
+    await page.screenshot({path: 'dist/consult_41963767.png'});
     await browser.close();
 })();
